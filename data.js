2309801f02d220fbf8c04651078c3568af3d66e8
Denis Sanchez Leyva <denisijcu266@gmail.com>
2018-11-24 15:48:54 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,147 @@
+const images = [
+    '../img/m1.png', '../img/m2.png', '../img/m3.png', '..img/a1.jpg', '../img/a2.jpg', '../img/a3.jpg',
+     '../img/fish.png', '../img/crab.jpg', '../img/dolphin.jpg'];
+
+let fp = `
+    
+     <p style = 'text-align: justify'> Fish can communicate in their underwater environments through the use of acoustic communication. 
+     Acoustic communication in fish involves the transmission of acoustic signals from one individual of 
+     a species to another. The production of sounds as a means of communication among fish is most often used 
+     in the context of feeding, aggression or courtship behaviour.[5] The sounds emitted by fish can vary
+     depending on the species and stimulus involved. They can produce either stridulatory sounds by moving 
+     components of the skeletal system, or can produce non-stridulatory sounds by manipulating specialized
+     organs such as the swimbladder.</p>
+     <br>
+     <img src="${images[6]}" id="fishes" alt="fish" title="Fishes" />
+    <br>`;
+
+let gp = `
+   
+     <p> About 850 species of crab are freshwater, terrestrial or semi-terrestrial species;[7] 
+     they are found throughout the world's tropical and semi-tropical regions. They were previously
+     thought to be a monophyletic group, but are now believed to represent at least two distinct lineages,
+     one in the Old World and one in the New World. </p>
+     <br>
+     <img src="${images[7]}" id="fishes" alt="crab" title="Crab" />
+     <br>
+                               `;
+let dp = `
+ 
+    <p>Dolphins range in size from the 1.7 m (5.6 ft) long and 50 kg (110 lb) Maui's dolphin to the 9.5 m (31 ft) and 10t
+    (11 short tons) killer whale. Several species exhibit sexual dimorphism, in that the males are larger than females.
+    They have streamlined bodies and two limbs that are modified into flippers. Though not quite as flexible as seals, 
+    some dolphins can travel at 55.5 km/h (34.5 mph). Dolphins use their conical shaped teeth to capture fast moving prey. 
+    They have well-developed hearing which is adapted for both air and water and is so well developed that some can survive 
+    even if they are blind. Some species are well adapted for diving to great depths. They have a layer of fat, or blubber,
+    under the skin to keep warm in the cold water. </p>
+    <br>
+    <img src="${images[8]}" id="fishes" alt="dolphin" title="Dolphin" />
+    <br>`;
+
+let book = `
+<p>An ocean is a body of saline water that composes much of a planet's hydrosphere.
+On Earth, an ocean is one of the major conventional divisions of the World Ocean.
+These are, in descending order by area, the Pacific, Atlantic, Indian, Southern (Antarctic),
+and Arctic Oceans. The word "ocean" is often used interchangeably with "sea" in American English.
+Strictly speaking, a sea is a body of saline water (generally a division of the world ocean) partly 
+or fully enclosed by land,[5] though "the sea" refers also to the oceans.</p>
+<br>
+<img src="../img/fish.png" id="fishes" alt="fish" title="Fishes" />
+<br>
+<h3> Fish </h3>
+<p style = 'text-align: justify'><strong>Fish  </strong> are gill-bearing aquatic craniate animals that lack limbs with digits. 
+They form a sister group to the tunicates, together forming the olfactores. Included in this definition are the living hagfish, 
+lampreys, and cartilaginous and bony fish as well as various extinct related groups. Tetrapods emerged within lobe-finned fishes,
+so cladistically they are fish as well. However, traditionally fish are rendered paraphyletic by excluding the tetrapods
+(i.e., the amphibians, reptiles, birds and mammals which all descended from within the same ancestry). Because in this manner
+the term "fish" is defined negatively as a paraphyletic group, it is not considered a formal taxonomic grouping in systematic 
+biology, unless it is used in the cladistic sense, including tetrapods.[1][2] The traditional term pisces (also ichthyes) is
+considered a typological, but not a phylogenetic classification. </p>
+
+<p>Fish can communicate in their underwater environments through the use of acoustic communication. 
+Acoustic communication in fish involves the transmission of acoustic signals from one individual of 
+a species to another. The production of sounds as a means of communication among fish is most often used 
+in the context of feeding, aggression or courtship behaviour.[5] The sounds emitted by fish can vary
+depending on the species and stimulus involved. They can produce either stridulatory sounds by moving 
+components of the skeletal system, or can produce non-stridulatory sounds by manipulating specialized
+organs such as the swimbladder.</p>
+<br><br>
+<h3> Crabs </h3>
+<p> Crabs are decapod crustaceans of the infraorder Brachyura, which typically have a very short projecting "tail" 
+(abdomen) (Greek: βραχύς, translit. brachys = short,[2] οὐρά / οura = tail[3]), usually entirely hidden under the thorax.
+They live in all the world's oceans, in fresh water, and on land, are generally covered with a thick exoskeleton and
+have a single pair of claws. Many other animals with similar names – such as hermit crabs, king crabs, porcelain crabs,
+horseshoe crabs, and crab lice – are not true crabs.</p>
+<br>
+<img src="../img/crab.jpg" id="fishes" alt="crab" title="Crab" />
+<br><br>
+<p>Crabs are generally covered with a thick exoskeleton, composed 
+primarily of highly mineralized chitin, and armed with a single pair of chelae (claws).
+Crabs are found in all of the world's oceans, while many crabs live in fresh water and on land,
+particularly in tropical regions. Crabs vary in size from the pea crab, a few millimetres wide,
+to the Japanese spider crab, with a leg span of up to 4 metres (13 ft).
+
+About 850 species of crab are freshwater, terrestrial or semi-terrestrial species;[7] 
+they are found throughout the world's tropical and semi-tropical regions. They were previously
+thought to be a monophyletic group, but are now believed to represent at least two distinct lineages,
+one in the Old World and one in the New World.
+
+The earliest unambiguous crab fossils date from the Jurassic,[9] although Carboniferous Imocaris,
+known only from its carapace, may be a primitive crab.[10] The radiation of crabs in the Cretaceous
+and afterward may be linked either to the break-up of Gondwana or to the concurrent radiation 
+of bony fish, crabs' main predators.</p>
+<br>
+<h3> Dolphin </h3>
+<br><br><img src="../img/dolphin.jpg" id="fishes" alt="dolphin" title="Dolphin" /><br><br>
+
+<p>Dolphin is a common name of aquatic mammals within the order Cetacea,
+arbitrarily excluding whales and porpoises. The term dolphin usually refers to the extant families Delphinidae 
+(the oceanic dolphins), Platanistidae (the Indian river dolphins), Iniidae (the new world river dolphins), 
+and Pontoporiidae (the brackish dolphins), and the extinct Lipotidae (baiji or Chinese river dolphin).
+There are 40 extant species named as dolphins.
+
+Dolphins range in size from the 1.7 m (5.6 ft) long and 50 kg (110 lb) Maui's dolphin to the 9.5 m (31 ft) and 10
+t (11 short tons) killer whale. Several species exhibit sexual dimorphism, in that the males are larger than females.
+They have streamlined bodies and two limbs that are modified into flippers. Though not quite as flexible as seals, 
+some dolphins can travel at 55.5 km/h (34.5 mph). Dolphins use their conical shaped teeth to capture fast moving prey. 
+They have well-developed hearing which is adapted for both air and water and is so well developed that some can survive 
+even if they are blind. Some species are well adapted for diving to great depths. They have a layer of fat, or blubber,
+under the skin to keep warm in the cold water.
+
+Although dolphins are widespread, most species prefer the warmer waters of the tropic zones, but some, like the right whale dolphin,
+prefer colder climates. Dolphins feed largely on fish and squid, but a few, like the killer whale, feed on large mammals, like seals.
+Male dolphins typically mate with multiple females every year, but females only mate every two to three years. Calves are typically
+born in the spring and summer months and females bear all the responsibility for raising them. Mothers of some species fast and nurse
+their young for a relatively long period of time. Dolphins produce a variety of vocalizations, usually in the form of clicks and whistles.
+
+Dolphins are sometimes hunted in places like Japan, in an activity known as dolphin drive hunting. Besides drive hunting, they also face
+threats from bycatch, habitat loss, and marine pollution. Dolphins have been depicted in various cultures worldwide. Dolphins occasionally
+feature in literature and film, as in the film series Free Willy. Dolphins are sometimes kept in captivity and trained to perform tricks.
+The most common dolphin species kept is the bottlenose dolphin, while there are around 60 captive killer whales. </p>
+`;
+
+
+let data = [{
+    animal: 'Fish',
+    parraph: fp,
+    img: images[0]
+},
+{
+    animal: 'Grab',
+    parraph: gp,
+    img: images[1]
+}, {
+    animal: 'Dolphin',
+    parraph: dp,
+    img: images[2]
+},
+{
+    animal: 'Oceans animals',
+    parraph: book,
+    img: images[6]
+},
+
+];
+
+
+//console.log(data);
